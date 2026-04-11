6294bf1fe138744a5b55c26c6a846592b06f07f5
Landa2006 <landaverdeleydi81@gamil.com>
2026-04-11 16:33:03 -0600

    Parcial 02 Fundamentos de programación, Ejercicio 6 validación de texto

---
+++ Parcial2/Ejercicio6.py
@@ -0,0 +1,8 @@
+texto = "LeydiLandaverdeAlas"
+
+normalizado = texto.casefold()
+
+solo_letras = normalizado.isalpha()
+
+print("Texto normalizado:", normalizado)
+print("Solo letras:", solo_letras)
